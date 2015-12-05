bac54c4281b4e33ff70bd6ae06ae914febdf28e9
Sahana Subbanna <ss4737@columbia.edu>
2015-12-05 16:11:17 -0500

    Moving data collection to different folder

---
+++ DataCollection/config/access.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var sqsQueue = {
+	url: ""
+};
+
+//Keys for ATeam
+var awsKeys = {
+	accessKey: "AKIAJMY5U2UZVX75SC6A",
+	accessKeySecret: "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",
+	awsRegion : 'us-east-1'
+};
+
+// var googleMapsKeys = {
+// 	accessKey: "AIzaSyCsnqL6vjQkNBaK9BEpqwwvhBRaSr15Ckk"
+// };
+
+module.exports = { awsKeys:awsKeys, sqsQueue: sqsQueue };
