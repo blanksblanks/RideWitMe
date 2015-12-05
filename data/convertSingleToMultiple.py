c6aa2b0b3d1f1916d9db695e957127b47da6202c
nb2406@columbia.edu <nb2406@columbia.edu>
2015-12-05 15:40:17 -0500

    Update conversion script

--- data/convertSingleToMultiple.py
+++ data/convertSingleToMultiple.py
@@ -1,15 +1,38 @@
-csv_file = 'station_snapshots_20151129.csv'
-f = open(csv_file, "r");
+import sys
+import os.path
 
-Lines = f.readlines()
+def main():
+	csv_file = str(sys.argv[1])
+	# f = open(csv_file, "r")
 
-i = 0
-counter = 0;
-firstLine = Lines[0]
+	# Create subdirectory for split files
+	filename = os.path.splitext(csv_file)[0]
+	if not os.path.exists(filename):
+		os.makedirs(filename)
 
-for i in range(len(Lines)):
-	with open('filename{}.txt'.format(counter), 'w') as output:
-		output.write(firstLine);
-		output.write(Lines[i + 500])
-		counter = counter  + 1
-		print 'filename{}.txt'.format(counter)
+	N = 5000
+	counter = 0
+
+	with open(csv_file, 'r') as infile:
+	    first_line = infile.readline()
+	    lines = []
+	    for line in infile:
+	        lines.append(line)
+	        if len(lines) > N:
+				counter = process(lines, first_line, counter, filename)
+				print counter
+				lines = []
+	    if len(lines) > 0:
+	        process(lines)
+
+def process(lines, first_line, counter, filename):
+	write_file = '{}/{}_{}.csv'.format(filename, filename, counter)
+	output = open(write_file, 'w')
+	output.write(first_line)
+	for line in lines:
+		output.write(line)
+	print write_file
+	return counter + 1
+
+if __name__ == "__main__":
+    main()
