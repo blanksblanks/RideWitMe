e761dc0a42ee80268e5702a8434d17679ac4f3a8
Natasha Kenkre <natsk@dyn-160-39-194-192.dyn.columbia.edu>
2015-12-05 13:49:19 -0500

    add single to multiple text files py script

---
+++ convertSingleToMultiple.py
@@ -0,0 +1,14 @@
+
+f = open('station_snapshots_text.txt', 'r')
+
+Lines = f.readlines()
+
+i = 0
+counter = 0;
+firstLine = Lines[0]
+
+for i in range(len(Lines)):
+	with open('filename{}.txt'.format(counter), 'w') as output:
+		output.write(firstLine);
+		output.write(Lines[i + 500])
+		counter = counter  + 1
